5c17b2b264c09c5c953eaf2efc87617815137377
Samuele <samuelebonato96@gmail.com>
2017-01-17 15:16:41 +0100

    Aggiunte MainActivity e ProfileCreationActivity

--- app/src/main/java/com/lynxspa/androidadvanced201617/MainActivity.java
+++ app/src/main/java/com/lynxspa/androidadvanced201617/MainActivity.java
@@ -1,13 +1,25 @@
 package com.lynxspa.androidadvanced201617;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+private Button addProfile;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        addProfile= (Button) findViewById(R.id.buttonAddProfile);
+        addProfile.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent = new Intent(this, ProfileCreationActivity.class);
+        startActivity(intent);
+        this.finish();
     }
 }
---
+++ app/src/main/java/com/lynxspa/androidadvanced201617/ProfileCreationActivity.java
@@ -0,0 +1,13 @@
+package com.lynxspa.androidadvanced201617;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ProfileCreationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_creation);
+    }
+}
