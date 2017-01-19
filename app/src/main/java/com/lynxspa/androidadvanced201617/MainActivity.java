fb1f1611aeba4923113381e8a69b6925c4c29427
mohamed_kisho <nomail@mail.com>
2017-01-19 15:54:41 +0100

    test commit

--- app/src/main/java/com/lynxspa/androidadvanced201617/MainActivity.java
+++ app/src/main/java/com/lynxspa/androidadvanced201617/MainActivity.java
@@ -1,23 +1,31 @@
 package com.lynxspa.androidadvanced201617;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    DBHelper mydb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String[] profili={"Casa","Lavoro","Accademia"};
+        mydb = DBHelper.getInstance(this);
+
+
+        ArrayList<String> profili=new ArrayList<String>();
         ListView listView=(ListView)findViewById(R.id.listView);
         ArrayAdapter<String> adapter=new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,profili);
 
@@ -26,14 +34,22 @@ public class MainActivity extends AppCompatActivity {
         addProfileButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View arg0) {
-                Intent editActivity = new Intent(MainActivity.this,ProfileDetail.class);
+                Intent editActivity = new Intent(MainActivity.this, ProfileDetail.class);
+                //  editActivity.putStringArrayListExtra("listaProfili", profili);
                 startActivity(editActivity);
+
             }
+
         });
-
-
+        if(getIntent().getStringExtra("profilo")!=null) {
+            String profilo = getIntent().getStringExtra("profilo").toString();
+            Toast.makeText(getApplicationContext(), profilo+" aggiunto", Toast.LENGTH_SHORT).show();
+            profili.add(profilo);
+        }
+        else{
+            return;
+        }
     }
-
 
 
 
--- app/src/main/java/com/lynxspa/androidadvanced201617/ProfileDetail.java
+++ app/src/main/java/com/lynxspa/androidadvanced201617/ProfileDetail.java
@@ -3,23 +3,30 @@ package com.lynxspa.androidadvanced201617;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.SeekBar;
+import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class ProfileDetail extends AppCompatActivity {
 
+    DBHelper mydb;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
 
+        EditText editText=(EditText) findViewById(R.id.nomeProfilo);
 
-
-
-        Button gpsButton=(Button) findViewById(R.id.gpsButton);
+        /*Button gpsButton=(Button) findViewById(R.id.gpsButton);
         Button wifiButton=(Button) findViewById(R.id.wifiButton);
         Button nfcButton=(Button) findViewById(R.id.nfcButton);
         Button beaconButton=(Button) findViewById(R.id.beaconButton);
@@ -28,23 +35,41 @@ public class ProfileDetail extends AppCompatActivity {
         buttons.add(gpsButton);
         buttons.add(wifiButton);
         buttons.add(nfcButton);
-        buttons.add(beaconButton);
+        buttons.add(beaconButton);*/
+        RadioGroup radioGroup=(RadioGroup)findViewById(R.id.radioGroup);
 
-        creaProfilo();
+        SeekBar brightnessBar=(SeekBar)findViewById(R.id.brightnessBar);
+
+        CheckBox checkBox=(CheckBox)findViewById(R.id.brightnessCheckBox);
+
+        SeekBar volumeBar=(SeekBar)findViewById(R.id.volumeBar);
+
+        Switch bluetooth=(Switch)findViewById(R.id.switchBluetooth);
+        Switch wifi=(Switch)findViewById(R.id.switchWifi);
+
+        creaProfilo(editText, radioGroup,brightnessBar,checkBox, volumeBar, bluetooth, wifi);
+
     }
 
-    private void creaProfilo(){
+    private void creaProfilo(final EditText editText, final RadioGroup buttons, final SeekBar brightnessBar, final CheckBox brightnessCheckBox,
+                             final SeekBar volumeBar, final Switch bluetooth, final Switch wifi){
 
-        final TextView textView=(TextView) findViewById(R.id.nomeProfilo);
         Button confirmButton=(Button)findViewById(R.id.confirmButton);
-        confirmButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View arg0) {
-                Intent mainActivity = new Intent(ProfileDetail.this,MainActivity.class);
-                mainActivity.putExtra("nomeProfilo",textView.getText());
-                startActivity(mainActivity);
-            }
-        });
+        mydb = DBHelper.getInstance(this);
+             confirmButton.setOnClickListener(new View.OnClickListener() {
+                 @Override
+                 public void onClick(View arg0) {
+                     if (editText.getText().toString() != null && !editText.getText().toString().equals("")) {
+                         mydb.addProfile(editText.toString(),buttons,brightnessBar,brightnessCheckBox,volumeBar,bluetooth,wifi);
+                     } else {
+                         Toast.makeText(getApplicationContext(), "Errore campo vuoto, inserire un profilo valido", Toast.LENGTH_SHORT).show();
+                    }
+                     finish();
+                }
+            });
+
+
+
 
     }
 
