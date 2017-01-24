c60422496212e2c17f53763ca14592e6afe58aec
mohamed_kisho <nomail@mail.com>
2017-01-24 13:04:16 +0100

    test commit

--- app/src/main/java/com/lynxspa/androidadvanced201617/MainActivity.java
+++ app/src/main/java/com/lynxspa/androidadvanced201617/MainActivity.java
@@ -1,13 +1,18 @@
 package com.lynxspa.androidadvanced201617;
 
+import android.app.AlertDialog;
+import android.app.Application;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -24,10 +29,14 @@ public class MainActivity extends AppCompatActivity {
 
         mydb = DBHelper.getInstance(this);
 
+    }
 
-       // ArrayList<String> profili=new ArrayList<String>();
+    @Override
+    protected void onResume(){
+        super.onResume();
+        // ArrayList<String> profili=new ArrayList<String>();
         ListView listView=(ListView)findViewById(R.id.listView);
-        ArrayAdapter<String> adapter=new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mydb.getAllProfiles());
+        ArrayAdapter<String> adapter=new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,mydb.getAllProfiles() );
 
         listView.setAdapter(adapter);
         Button addProfileButton=(Button)findViewById(R.id.addProfile);
@@ -41,23 +50,37 @@ public class MainActivity extends AppCompatActivity {
             }
 
         });
-       /* if(getIntent().getStringExtra("profilo")!=null) {
-            String profilo = getIntent().getStringExtra("profilo").toString();
-            Toast.makeText(getApplicationContext(), profilo+" aggiunto", Toast.LENGTH_SHORT).show();
-            profili.add(profilo);
-        }
-        else{
-            return;
-        }*/
+
+       /* listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent modifyProfile = new Intent(MainActivity.this, ModifyProfile.class);
+                modifyProfile.putStringArrayListExtra("listaProfili", mydb.getAllProfiles());
+                startActivity(modifyProfile);
+            }
+        });
+      /*  listView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final CharSequence menu[] = new CharSequence[] {"Modifica", "Elimina"};
+
+                final AlertDialog.Builder builder = new AlertDialog.Builder(new Application());
+                builder.setTitle("Seleziona un'opzione");
+                builder.setItems(menu, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent editActivity = new Intent(MainActivity.this, ProfileDetail.class);
+                        if (builder.getContext().getApplicationContext().equals("Modifica")) {
+                            //  editActivity.putStringArrayListExtra("listaProfili", profili);
+                            startActivity(editActivity);
+                        }else{
+                            return;
+                        }
+                    }
+                });
+                builder.show();
+            }
+        });*/
+
     }
-
-
-
-    private void visualizzaProfilo(String[] profili){
-
-    }
-
-
-
-
 }
