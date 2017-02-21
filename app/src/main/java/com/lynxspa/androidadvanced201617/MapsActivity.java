5ec200d66837f895535b1fe2ac366be20522f6ba
mohamed_kisho <nomail@mail.com>
2017-02-21 13:34:48 +0100

    Merge branch 'KIMO' of https://github.com/nbarresi/AndroidAdvanced201617 into KIMO
    # Conflicts:
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/com/lynxspa/androidadvanced201617/MapsActivity.java

--- app/src/main/java/com/lynxspa/androidadvanced201617/MapsActivity.java
+++ app/src/main/java/com/lynxspa/androidadvanced201617/MapsActivity.java
@@ -69,9 +69,39 @@ public class MapsActivity extends FragmentActivity implements  OnMapReadyCallbac
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
+        LatLng myPosition = new LatLng(0, 0);
+        //LatLng myPosition = new LatLng(dLat,dLong);
         // Add a marker in Sydney, Australia, and move the camera.
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+         return;
+        }
+        mMap.setMyLocationEnabled(true);
+        LocationManager locationManager= (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+
+        Location userLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        if (userLocation != null) {
+            myPosition = new LatLng(userLocation.getLatitude(),
+                    userLocation.getLongitude());
+            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(myPosition,
+                    mMap.getMaxZoomLevel() - 5));
+
+
+
+
+
+            //Circle circle = mMap.addCircle(new CircleOptions().center(new LatLng(dLat, dLong)).radius(1000));
+            Circle circle = mMap.addCircle(new CircleOptions().center(new LatLng(userLocation.getLatitude(),userLocation.getLongitude() )).radius(1000).strokeColor(Color.RED));
+
+
+            mMap.addMarker(new MarkerOptions().position(myPosition).title("My position"));
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(myPosition));
+        }
     }
 }
